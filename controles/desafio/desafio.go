c466de8d235cfd11a0a2b8bbae1e97f4fa318a43
Celso Bertolotto <celsonyl@hotmail.com>
2021-09-16 14:04:54 -0300

    desafio switch

---
+++ controles/desafio/desafio.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(notaParaConceito(2))
+	fmt.Println(notaParaConceito(9))
+	fmt.Println(notaParaConceito(5))
+}
+
+func notaParaConceito(nota float64) string {
+	switch {
+	case nota >= 9 && nota <= 10:
+		return "A"
+	case nota >= 8 && nota < 9:
+		return "B"
+	case nota >= 5 && nota < 8:
+		return "C"
+	case nota >= 3 && nota < 5:
+		return "D"
+	case nota >= 0 && nota < 3:
+		return "E"
+	default:
+		return "Nota inválida!"
+	}
+}
